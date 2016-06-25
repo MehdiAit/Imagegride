5f52e2c67b5cda46fefc1f1cd451af82cd5e4e6c
Mehdi Ait younes <overpex@gmail.com>
2016-06-25 13:48:08 -0400

    Initial commit

---
+++ app/src/main/java/com/example/mehdi/imagegride/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.mehdi.imagegride;
+
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView imgGride = (GridView) findViewById(R.id.gridview);
+        imgGride.setAdapter(new Myadapter(this));
+
+        imgGride.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l) {
+                Toast tst = new Toast(MainActivity.this);
+                ImageView test = new ImageView(MainActivity.this);
+
+                ImageView img = (ImageView) view;
+                Drawable myDrawable = img.getDrawable();
+
+                test.setImageDrawable(myDrawable);
+                tst.setView(test);
+                tst.setGravity(Gravity.FILL, 0, 0);
+                tst.show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/mehdi/imagegride/Myadapter.java
@@ -0,0 +1,43 @@
+package com.example.mehdi.imagegride;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+/**
+ * Created by antonin on 16-06-23.
+ */
+public class Myadapter extends BaseAdapter {
+    Context currentcontext;
+
+    public Myadapter (Context context){
+        currentcontext = context;
+    }
+
+    @Override
+    public int getCount() {
+        return 30;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView img;
+        img = new ImageView(currentcontext);
+        img.setLayoutParams(new GridView.LayoutParams(250, 250));
+        img.setImageResource(R.drawable.test);
+        return img;
+    }
+}
